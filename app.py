bdb0bc9aeb9ba57714549fa8539344a8b1847200
anuragsworld1-oss <anuragsworld1@gmail.com>
2026-05-09 12:51:17 +0530

    Improved UI

--- app.py
+++ app.py
@@ -2,35 +2,94 @@ import pickle
 import streamlit as st
 import requests
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+st.set_page_config(
+    page_title="Movie Recommender",
+    page_icon="🎬",
+    layout="wide"
+)
+
+
+st.markdown(
+    """
+    <style>
+    .main {
+        background-color: #0E1117;
+    }
+
+    h1 {
+        text-align: center;
+        color: white;
+        font-size: 50px;
+    }
+
+    .stButton>button {
+        width: 100%;
+        border-radius: 10px;
+        height: 3em;
+        background-color: #E50914;
+        color: white;
+        font-size: 18px;
+        border: none;
+    }
+
+    .stButton>button:hover {
+        background-color: #ff2e2e;
+        color: white;
+    }
+
+    .movie-title {
+        text-align: center;
+        font-size: 18px;
+        font-weight: bold;
+        color: white;
+        margin-top: 10px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US"
+
+    data = requests.get(url).json()
+
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
+
+    return "https://via.placeholder.com/500x750?text=No+Image"
+
 
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+
+    distances = sorted(
+        list(enumerate(similarity[index])),
+        reverse=True,
+        key=lambda x: x[1]
+    )
+
     recommended_movie_names = []
     recommended_movie_posters = []
+
     for i in distances[1:6]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
+
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names,recommended_movie_posters
+    return recommended_movie_names, recommended_movie_posters
 
-
-st.header('Movie Recommender System')
-movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
 
 cv = CountVectorizer(max_features=5000, stop_words='english')
 
@@ -38,29 +97,51 @@ vectors = cv.fit_transform(movies['tags']).toarray()
 
 similarity = cosine_similarity(vectors)
 
-movie_dict = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_dict
+st.markdown("<h1>🎬 Movie Recommender System</h1>", unsafe_allow_html=True)
+
+st.markdown(
+    "<p style='text-align:center; color:gray;'>Find movies similar to your favorite ones 🍿</p>",
+    unsafe_allow_html=True
 )
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+movie_list = movies['title'].values
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+selected_movie = st.selectbox(
+    "Choose a movie",
+    movie_list
+)
 
+
+if st.button('Recommend Movies'):
+
+    with st.spinner('Finding best recommendations for you...'):
+        recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            st.image(recommended_movie_posters[0])
+            st.markdown(f"<div class='movie-title'>{recommended_movie_names[0]}</div>", unsafe_allow_html=True)
+
+        with col2:
+            st.image(recommended_movie_posters[1])
+            st.markdown(f"<div class='movie-title'>{recommended_movie_names[1]}</div>", unsafe_allow_html=True)
+
+        with col3:
+            st.image(recommended_movie_posters[2])
+            st.markdown(f"<div class='movie-title'>{recommended_movie_names[2]}</div>", unsafe_allow_html=True)
+
+        with col4:
+            st.image(recommended_movie_posters[3])
+            st.markdown(f"<div class='movie-title'>{recommended_movie_names[3]}</div>", unsafe_allow_html=True)
+
+        with col5:
+            st.image(recommended_movie_posters[4])
+            st.markdown(f"<div class='movie-title'>{recommended_movie_names[4]}</div>", unsafe_allow_html=True)
+
+
+st.markdown("---")
+st.markdown(
+    "<p style='text-align:center; color:gray;'>Built with Streamlit ❤️</p>",
+    unsafe_allow_html=True
+)
